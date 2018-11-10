e188e7048505414d00e4afa391f06a6df732e679
s173046 <s173046@matsu.shimane-u.ac.jp>
2018-11-10 02:23:40 -0500

    コメントのControllerを作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,28 @@
 class CommentsController < ApplicationController
-
+  before_action :set_comment, only: [:destory]
+  
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.build(comment_params)
+    
+    if @comment.save
+      redirect_to article_path(@article)
+    else
+      render "articles/show"
+    end
+  end
+  
+  def destroy
+    @comment.destroy
+    redirect_to request.referrer || root_url
+  end
+  
+  private
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+  
+    def comment_params
+      params.require(:comment).parmit(:body)
+    end
 end
